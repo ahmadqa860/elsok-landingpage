6d14a41bf2e1f118424eb4468d49ae53d1a88e8b
Shogun <ahmad.k.2841988@gmail.com>
2021-02-28 19:47:02 +0200

    css

--- src/components/utils/categoryList.jsx
+++ src/components/utils/categoryList.jsx
@@ -10,35 +10,35 @@ const CategoryList = () => {
                     <div className="col-lg-4 col-sm-6">
                         <a className="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg"><img className="img-fluid" src={img1} alt=""/>
                             <div className="portfolio-box-caption">
-                                <div className="project-category text-white-50">Category</div>
+                                <div className="project-category text-white-50">سيارات</div>
                                 <div className="project-name">Project Name</div>
                             </div></a>
                     </div>
                     <div className="col-lg-4 col-sm-6">
                         <a className="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg"><img className="img-fluid" src={img1} alt=""/>
                             <div className="portfolio-box-caption">
-                                <div className="project-category text-white-50">Category</div>
+                                <div className="project-category text-white-50">الالكترونيات</div>
                                 <div className="project-name">Project Name</div>
                             </div></a>
                     </div>
                     <div className="col-lg-4 col-sm-6">
                         <a className="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg"><img className="img-fluid" src={img1} alt=""/>
                             <div className="portfolio-box-caption">
-                                <div className="project-category text-white-50">Category</div>
+                                <div className="project-category text-white-50">الكمبيوتر ومستلزماته</div>
                                 <div className="project-name">Project Name</div>
                             </div></a>
                     </div>
                     <div className="col-lg-4 col-sm-6">
                         <a className="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg"><img className="img-fluid" src={img1} alt=""/>
                             <div className="portfolio-box-caption">
-                                <div className="project-category text-white-50">Category</div>
+                                <div className="project-category text-white-50">الموضة</div>
                                 <div className="project-name">Project Name</div>
                             </div></a>
                     </div>
                     <div className="col-lg-4 col-sm-6">
                         <a className="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg"><img className="img-fluid" src={img1} alt=""/>
                             <div className="portfolio-box-caption">
-                                <div className="project-category text-white-50">Category</div>
+                                <div className="project-category text-white-50">عقارات</div>
                                 <div className="project-name">Project Name</div>
                             </div></a>
                     </div>
